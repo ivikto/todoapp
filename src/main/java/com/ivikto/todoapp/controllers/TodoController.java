e0cfe025a10f28466b22d9f08c792ba3d02c8310
ivikto <i.zhabchenko@yandex.ru>
2025-02-06 16:49:50 +0300

    Finish project

--- src/main/java/com/ivikto/todoapp/controllers/TodoController.java
+++ src/main/java/com/ivikto/todoapp/controllers/TodoController.java
@@ -6,7 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -23,9 +27,46 @@ public class TodoController implements CommandLineRunner {
 
         List<Todoitem> allTodos = todoItemRepository.findAll();
         model.addAttribute("allTodos", allTodos);
+        model.addAttribute("newTodo", new Todoitem());
 
         return "index";
     }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute Todoitem todoitem) {
+        todoItemRepository.save(todoitem);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/del/{id}")
+    public String del(@ModelAttribute Todoitem todoitem) {
+        todoItemRepository.delete(todoitem);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String searchTodoItems(@RequestParam("searchTerm") String searchTerm, Model model) {
+        List<Todoitem> allItems = todoItemRepository.findAll();
+        List<Todoitem> searchResults = new ArrayList<>();
+
+        for (Todoitem item : allItems) {
+            if (item.getTitle().toLowerCase().contains(searchTerm.toLowerCase())) {
+                searchResults.add(item);
+            }
+        }
+
+        model.addAttribute("allTodos", searchResults);
+        model.addAttribute("newTodo", new Todoitem());
+        model.addAttribute("searchTerm", searchTerm);
+
+        return "index";
+
+
+
+    }
+
 
     @Override
     public void run(String... args) throws Exception {
